28b16bcbb82c57185df272ea50a59d3fd6d009f9
Your Name <you@example.com>
2021-10-17 20:34:58 +0300

    set up complete

---
+++ reactproject/src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Switch } from "react-router";
+import axios from "axios";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
